4ec429d52e80b8ef6c95d718234b27d27b4f107b
Antônio Ximenes <almeid.ximenes@gmail.com>
2025-01-08 02:07:45 -0300

    Criando api de produto

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,20 +1,22 @@
+// pages/cart.tsx
+
 import { NextPage } from "next";
 import Head from "next/head";
 
 const Cart: NextPage = () => {
-    return (
-        <>
-            <Head>
-                <title>Carrinho</title>
-                <meta name="description" content="Meu carrinho de compras" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+  return (
+    <>
+      <Head>
+        <title>Carrinho</title>
+        <meta name="description" content="Meu carrinho de compras" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-            {/* adicionando o conteudo */}
-            <main>
-                <h3>Carrinho</h3>
-            </main>
-        </>
-    )}
+      <h1>
+        Carrinho
+      </h1>
+    </>
+  )
+}
 
-    export default Cart
+export default Cart
